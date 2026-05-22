c5e7c02497048cb5c6326342d24a67c13a89e179
Jonghun Lim <jhlim00812@gmail.com>
2026-05-23 03:14:33 +0900

    Feat(instructor): Instructor 엔티티 및 레포지토리 추가

---
+++ src/main/java/ditda/backend/domain/instructor/auth/entity/Instructor.java
@@ -0,0 +1,40 @@
+package ditda.backend.domain.instructor.auth.entity;
+
+import ditda.backend.domain.common.user.entity.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "instructors")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Instructor {
+
+	@Id
+	@Column(name = "instructor_id")
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "instructor_id")
+	private UserEntity user;
+
+	public static Instructor createInstructor(UserEntity user) {
+		return Instructor.builder()
+			.user(user)
+			.build();
+	}
+}
