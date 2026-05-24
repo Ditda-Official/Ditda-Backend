a110f74af77fd1bcae7b14bb10506e8579ce4fd1
fervovita <rrvvy9595@gmail.com>
2026-05-24 23:50:39 +0900

    Feat(designer): 디자이너 도메인 모델 추가

---
+++ src/main/java/ditda/backend/domain/designer/auth/entity/Portfolio.java
@@ -0,0 +1,46 @@
+package ditda.backend.domain.designer.auth.entity;
+
+import ditda.backend.domain.common.user.entity.User;
+import ditda.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "portfolios")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Portfolio extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "portfolio_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(name = "portfolio_url", nullable = false)
+	private String portfolioUrl;
+
+	public static Portfolio createPortfolio(User user, String portfolioUrl) {
+		return Portfolio.builder()
+			.user(user)
+			.portfolioUrl(portfolioUrl)
+			.build();
+	}
+}
