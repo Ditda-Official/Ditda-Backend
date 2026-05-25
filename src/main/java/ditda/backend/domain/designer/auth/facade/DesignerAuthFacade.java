9ee2b384b9cb39f6665bce928d73dc899e96fa9e
fervovita <rrvvy9595@gmail.com>
2026-05-25 23:57:38 +0900

    Feat(designer): 디자이너 회원가입시 아이디 및 이메일 검증 추가

--- src/main/java/ditda/backend/domain/designer/auth/facade/DesignerAuthFacade.java
+++ src/main/java/ditda/backend/domain/designer/auth/facade/DesignerAuthFacade.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import ditda.backend.domain.common.auth.service.EmailVerificationService;
+import ditda.backend.domain.common.user.service.UserService;
 import ditda.backend.domain.designer.auth.dto.DesignerAuthResult;
 import ditda.backend.domain.designer.auth.dto.request.DesignerSignupRequest;
 import ditda.backend.domain.designer.auth.service.DesignerAuthService;
@@ -19,11 +20,16 @@ public class DesignerAuthFacade {
 	private final DesignerAuthService designerAuthService;
 	private final EmailVerificationService emailVerificationService;
 	private final PortfolioService portfolioService;
+	private final UserService userService;
 
 	public DesignerAuthResult signup(DesignerSignupRequest request, List<MultipartFile> portfolioFiles) {
 
 		// 이메일 인증 여부 확인
 		emailVerificationService.validateVerified(request.email());
+
+		// 아이디 및 이메일 검증
+		userService.validateUsernameAvailable(request.username());
+		userService.validateEmailAvailable(request.email());
 
 		// 포트폴리오 파일 검증 및 S3 업로드
 		portfolioService.validateFiles(portfolioFiles);
