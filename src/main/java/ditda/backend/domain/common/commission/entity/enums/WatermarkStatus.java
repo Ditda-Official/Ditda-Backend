998153af377c759a703f5c8e2cf69d94225e0245
Jonghun Lim <jhlim00812@gmail.com>
2026-05-29 00:13:16 +0900

    Rename(commission): Enum 파일명 변경

--- src/main/java/ditda/backend/domain/common/commission/entity/enums/WatermarkStatus.java
+++ src/main/java/ditda/backend/domain/common/commission/entity/enums/WatermarkStatus.java
@@ -1,6 +1,6 @@
 package ditda.backend.domain.common.commission.entity.enums;
 
-public enum DraftStatus {
+public enum WatermarkStatus {
 	PROCESSING,		// 워터마크 처리 중
 	COMPLETED,		// 워터마크 처리 완료 및 강사 조회 가능
 	FAILED			// 처리 실패
