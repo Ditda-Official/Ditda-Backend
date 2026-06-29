4afe75f3901b58d43e98ae510bc8d5e754b3cee8
fervovita <rrvvy9595@gmail.com>
2026-06-30 00:57:54 +0900

    Docs(commission): 카테고리 상세 응답에 oneOf 스키마 추가

--- src/main/java/ditda/backend/domain/commission/core/dto/response/CategoryDetailResponse.java
+++ src/main/java/ditda/backend/domain/commission/core/dto/response/CategoryDetailResponse.java
@@ -1,7 +1,11 @@
 package ditda.backend.domain.commission.core.dto.response;
 
+import ditda.backend.domain.commission.category.textbook.dto.response.TextbookDetailResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "카테고리별 상세 정보")
+@Schema(
+	description = "카테고리별 상세 정보",
+	oneOf = {TextbookDetailResponse.class}        // 카테고리 추가시 여기에 추가
+)
 public interface CategoryDetailResponse {
 }
