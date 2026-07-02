4d3a0a74343c26f508a57c4c0bfb4a6c542ff97f
Jonghun Lim <jhlim00812@gmail.com>
2026-07-02 17:18:51 +0900

    Docs(Application): Step 주석 추가

--- src/main/java/ditda/backend/domain/commission/application/entity/enums/ApplicationStatus.java
+++ src/main/java/ditda/backend/domain/commission/application/entity/enums/ApplicationStatus.java
@@ -6,11 +6,18 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ApplicationStatus {
+	// Step 1 - 지원
 	PENDING("지원 완료"),
+
+	// Step 2 - 지원 결과
 	ASSIGNED("1차 시안 대상자"),
 	APPLICATION_REJECTED("지원 탈락"),
+
+	// Step 3 - 1차 시안 제출 여부
 	DRAFT_SUBMITTED("1차 시안 제출 완료"),
 	DRAFT_MISSED("1차 시안 미제출"),
+
+	// Step 4 - 최종 선택 여부
 	DRAFT_SELECTED("최종 선택"),
 	DRAFT_REJECTED("최종 탈락");
 
