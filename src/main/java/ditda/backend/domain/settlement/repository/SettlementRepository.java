d3c0a33a5770f823c695a3d3ac039181e94eceb1
Jonghun Lim <jhlim00812@gmail.com>
2026-07-02 18:10:53 +0900

    Feat(Settlement): 디자이너 마이페이지 외주 지급내역 조회 API 추가

--- src/main/java/ditda/backend/domain/settlement/repository/SettlementRepository.java
+++ src/main/java/ditda/backend/domain/settlement/repository/SettlementRepository.java
@@ -1,5 +1,7 @@
 package ditda.backend.domain.settlement.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,5 +17,18 @@ public interface SettlementRepository extends JpaRepository<Settlement, Long> {
 	long sumAmountByDesignerIdAndStatus(
 		@Param("designerId") Long designerId,
 		@Param("status") SettlementStatus status
+	);
+
+	@Query(value = "SELECT s from Settlement s "
+		+ "JOIN FETCH s.commission c "
+		+ "WHERE s.designer.id = :designerId "
+		+ "AND s.status = :status",
+		countQuery = "SELECT COUNT(s) FROM Settlement s "
+		+ "WHERE s.designer.id = :designerId "
+		+ "AND s.status = :status")
+	Page<Settlement> findByDesignerIdAndStatus(
+		@Param("designerId") Long designerId,
+		@Param("status") SettlementStatus status,
+		Pageable pageable
 	);
 }
--- src/main/java/ditda/backend/domain/settlement/service/SettlementService.java
+++ src/main/java/ditda/backend/domain/settlement/service/SettlementService.java
@@ -1,8 +1,11 @@
 package ditda.backend.domain.settlement.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ditda.backend.domain.settlement.entity.Settlement;
 import ditda.backend.domain.settlement.entity.enums.SettlementStatus;
 import ditda.backend.domain.settlement.repository.SettlementRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +16,22 @@ public class SettlementService {
 
 	private final SettlementRepository settlementRepository;
 
+	// 정산 완료된 외주 개수 조회
 	@Transactional(readOnly = true)
 	public long getTotalIncome(Long designerId) {
 		return settlementRepository.sumAmountByDesignerIdAndStatus(
 			designerId,
 			SettlementStatus.COMPLETED
+		);
+	}
+
+	// 정산 완료된 외주 조회
+	@Transactional(readOnly = true)
+	public Page<Settlement> getCompletedSettlements(Long designerId, Pageable pageable) {
+		return settlementRepository.findByDesignerIdAndStatus(
+			designerId,
+			SettlementStatus.COMPLETED,
+			pageable
 		);
 	}
 }
