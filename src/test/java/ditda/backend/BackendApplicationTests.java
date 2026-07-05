5671a9e669106c4009d1b9c0cd0ee96bc54ecb5d
fervovita <rrvvy9595@gmail.com>
2026-07-05 16:55:30 +0900

    Feat: Redisson 기반 분산 락 AOP 추가

--- src/test/java/ditda/backend/BackendApplicationTests.java
+++ src/test/java/ditda/backend/BackendApplicationTests.java
@@ -1,12 +1,17 @@
 package ditda.backend;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendApplicationTests {
+
+	@MockitoBean
+	private RedissonClient redissonClient;
 
 	@Test
 	void contextLoads() {
