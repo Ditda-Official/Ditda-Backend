2387bb676784dda1445c226bdf7c99f9aaa77685
fervovita <rrvvy9595@gmail.com>
2026-08-13 00:31:50 +0900

    Refactor(admin): adminId 파라미터 제거

--- src/main/java/ditda/backend/domain/admin/designer/controller/AdminDesignerController.java
+++ src/main/java/ditda/backend/domain/admin/designer/controller/AdminDesignerController.java
@@ -1,6 +1,5 @@
 package ditda.backend.domain.admin.designer.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +24,10 @@ public class AdminDesignerController {
 	@Operation(summary = "디자이너 계좌 정보 조회", description = "**[어드민]** 정산 처리를 위해 디자이너의 계좌 정보를 조회합니다.")
 	@GetMapping("/{designerId}/account")
 	public ApiResponse<DesignerAccountResponse> getDesignerAccount(
-		@AuthenticationPrincipal Long adminId,
 		@PathVariable Long designerId
 	) {
 
-		DesignerAccountResponse response = adminDesignerService.getDesignerAccount(adminId, designerId);
+		DesignerAccountResponse response = adminDesignerService.getDesignerAccount(designerId);
 
 		return ApiResponse.onSuccess("디자이너 계좌 정보 조회 성공", response);
 	}
@@ -37,11 +35,10 @@ public class AdminDesignerController {
 	@Operation(summary = "디자이너 포트폴리오 조회", description = "**[어드민]** 디자이너가 가입 시 첨부한 포트폴리오의 presigned URL을 조회합니다.")
 	@GetMapping("/{designerId}/portfolios")
 	public ApiResponse<DesignerPortfolioResponse> getDesignerPortfolios(
-		@AuthenticationPrincipal Long adminId,
 		@PathVariable Long designerId
 	) {
 
-		DesignerPortfolioResponse response = adminDesignerService.getDesignerPortfolios(adminId, designerId);
+		DesignerPortfolioResponse response = adminDesignerService.getDesignerPortfolios(designerId);
 
 		return ApiResponse.onSuccess("디자이너 포트폴리오 조회 성공", response);
 	}
